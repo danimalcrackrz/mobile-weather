5a26b718afe70259db893c837cec9970b9b96d40
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-06-20 10:11:51 -0500

    - update to search component - refactor of temperature component - update to useGetTemperature hook

--- components/Search.js
+++ components/Search.js
@@ -1,38 +1,21 @@
-import {
-  KeyboardAvoidingView,
-  Text,
-  TextInput,
-  TouchableOpacity,
-} from 'react-native'
-import React, { useState } from 'react'
+import { View, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
 import { useRecoilState } from 'recoil'
-import { useGetTemperature } from '../utils/useGetTemperature'
-import { Ionicons } from '@expo/vector-icons'
 import { QUERYSTATE } from '../atoms'
 
 const Search = () => {
   const [query, setQuery] = useRecoilState(QUERYSTATE)
-  const [searchVisible, setSearchVisible] = useState(false)
-
-  const handleSearch = () => {
-    setSearchVisible(!searchVisible)
-  }
 
   return (
-    <KeyboardAvoidingView className='mb-4 flex-row items-center justify-end'>
-      {searchVisible && (
-        <TextInput
-          placeholder='Enter a City...'
-          keyboardAppearance='dark'
-          value={query}
-          onChangeText={setQuery}
-          className='border-2 p-4 rounded-full w-5/6 mr-2'
-        />
-      )}
-      <TouchableOpacity className='p-4' onPress={handleSearch}>
-        <Ionicons name='search' size={30} color='black' />
-      </TouchableOpacity>
-    </KeyboardAvoidingView>
+    <View className='mb-4 flex-row items-center'>
+      <TextInput
+        placeholder='Enter a City...'
+        keyboardAppearance='dark'
+        value={query}
+        onChangeText={setQuery}
+        className='border-2 p-4 rounded-full w-full'
+      />
+    </View>
   )
 }
 
