c9fbd05db6ff7fed0c6b0a3d3a02815612c6cdfc
Daniel Shaw <18403912+danimalcrackrz@users.noreply.github.com>
2023-06-21 21:18:51 -0500

    - background color change - added icon expanded forecast - added icons to summary - moved search component to top

--- components/Summary.js
+++ components/Summary.js
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Fontisto } from '@expo/vector-icons'
 import { useRecoilValue } from 'recoil'
 import { WEATHERSTATE, FORECASTSTATE } from '../atoms'
 
@@ -10,13 +11,32 @@ const Summary = () => {
   return (
     <View className='p-2'>
       <Text className='text-2xl font-semibold'>Daily Summary</Text>
-      <Text className='text-lg'>
-        Currently it feels like {current?.feelslike_f.toFixed(0)}&deg;.
-      </Text>
-      <Text className='text-lg'>
-        The high for today is currently{' '}
-        {weekForecast?.day?.maxtemp_f.toFixed(0)}&deg;.
-      </Text>
+      <View className='p-2'>
+        <Text className='text-lg'>
+          Currently it feels like {current?.feelslike_f.toFixed(0)}&deg;.
+        </Text>
+        <Text className='text-lg'>
+          The high for today is currently{' '}
+          {weekForecast?.day?.maxtemp_f.toFixed(0)}&deg;.
+        </Text>
+      </View>
+      <View className='bg-black flex-row justify-around p-6 rounded-3xl'>
+        <View className='p-2 items-center gap-2'>
+          <Fontisto name='blood-drop' size={30} color='white' />
+          <Text className='text-white text-xl'>{current?.humidity} %</Text>
+          <Text className='text-white text-base'>Humidity</Text>
+        </View>
+        <View className='p-2 items-center gap-2'>
+          <Fontisto name='wind' size={30} color='white' />
+          <Text className='text-white text-xl'>{current?.wind_mph} MPH</Text>
+          <Text className='text-white text-base'>Wind</Text>
+        </View>
+        <View className='p-2 items-center gap-2'>
+          <Fontisto name='eye' size={30} color='white' />
+          <Text className='text-white text-xl'>{current?.vis_miles} Miles</Text>
+          <Text className='text-white text-base'>Visibility</Text>
+        </View>
+      </View>
     </View>
   )
 }
